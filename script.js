9b323ca5dee395406392ee17006c2f0cb2a652e4
Sander Nijssen <nijssensander@gmail.com>
2017-01-20 21:26:24 +0100

    Naming + adding double digits
    Functionality for double digits in JS & switch case for HTML

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ function updateTime() {
     var hour = today.getHours();
     var mins = today.getMinutes();
     var secs = today.getSeconds();
+    if (secs <=9){
+           secs = "0" + secs;
+       }
+    if (mins <=9){
+           mins = "0" + mins;
+       }
+    if (hour <=9){
+           hour = "0" + hour;
+       }
     var totalTime = hour + ":" + mins + ":" + secs;
     setTimeout("updateTime()", 1000);
     console.log(totalTime);
