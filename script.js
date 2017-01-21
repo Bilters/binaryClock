d957450c763e69c97cfa57bb49dce9d762aa63b1
Sander Nijssen <nijssensander@gmail.com>
2017-01-21 17:56:07 +0100

    Starting looping
    Starting to figure out the looping of the clock

--- script.js
+++ script.js
@@ -3,16 +3,33 @@ function updateTime() {
     var hour = today.getHours();
     var mins = today.getMinutes();
     var secs = today.getSeconds();
-    if (secs <=9){
-           secs = "0" + secs;
-       }
-    if (mins <=9){
-           mins = "0" + mins;
-       }
-    if (hour <=9){
-           hour = "0" + hour;
-       }
+    if (secs <= 9) {
+        secs = "0" + secs;
+    }
+    if (mins <= 9) {
+        mins = "0" + mins;
+    }
+    if (hour <= 9) {
+        hour = "0" + hour;
+    }
     var totalTime = hour + ":" + mins + ":" + secs;
     setTimeout("updateTime()", 1000);
     console.log(totalTime);
+
+    function updateSecondsLast() {
+        // Isolating last number
+        secs = secs % 10;
+
+        if (secs === 0) {
+            document.secondsLast.s01.checked = false;
+            document.secondsLast.s02.checked = false;
+            document.secondsLast.s04.checked = false;
+            document.secondsLast.s08.checked = false;
+        } else {
+            // LOOP through numbers higher lower checked
+            
+        }
+
+        updateSecondsLast();
+    }
 }
