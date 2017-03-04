9965e0437d7626e2092ad1e668239809dc8809eb
Sander Nijssen <nijssensander@gmail.com>
2017-03-04 18:24:48 +0100

    Last numbers work
    kind of

--- script.js
+++ script.js
@@ -18,18 +18,128 @@ function updateTime() {
 
     function updateSecondsLast() {
         // Isolating last number
-        secs = secs % 10;
+        lastSecs = secs % 10;
+        selector1 = document.querySelector('#s01');
+        selector2 = document.querySelector('#s02');
+        selector4 = document.querySelector('#s04');
+        selector8 = document.querySelector('#s08');
+        switchCaseNumbers9(lastSecs);
+    }
 
-        if (secs === 0) {
-            document.secondsLast.s01.checked = false;
-            document.secondsLast.s02.checked = false;
-            document.secondsLast.s04.checked = false;
-            document.secondsLast.s08.checked = false;
-        } else {
-            // LOOP through numbers higher lower checked
-            
+    function updateSecondsFirst() {
+        firstSecs = secs / 10;
+        selector1 = document.querySelector('#s10');
+        selector2 = document.querySelector('#s20');
+        selector4 = document.querySelector('#s40');
+        switchCaseNumbers6(firstSecs);
+    }
+    updateSecondsLast();
+    updateSecondsFirst();
+
+}
+
+function switchCaseNumbers9(time) {
+    switch (time) {
+        case 1:
+            selector1.checked = true;
+            selector2.checked = false;
+            selector4.checked = false;
+            selector8.checked = false;
+            break;
+        case 2:
+            selector1.checked = false;
+            selector2.checked = true;
+            selector4.checked = false;
+            selector8.checked = false;
+            break;
+        case 3:
+            selector1.checked = true;
+            selector2.checked = true;
+            selector4.checked = false;
+            selector8.checked = false;
+            break;
+        case 4:
+            selector1.checked = false;
+            selector2.checked = false;
+            selector4.checked = true;
+            selector8.checked = false;
+            break;
+        case 5:
+            selector1.checked = true;
+            selector2.checked = false;
+            selector4.checked = true;
+            selector8.checked = false;
+            break;
+        case 6:
+            selector1.checked = false;
+            selector2.checked = true;
+            selector4.checked = true;
+            selector8.checked = false;
+            break;
+        case 7:
+            selector1.checked = true;
+            selector2.checked = true;
+            selector4.checked = true;
+            selector8.checked = false;
+            break;
+        case 8:
+            selector1.checked = false;
+            selector2.checked = false;
+            selector4.checked = false;
+            selector8.checked = true;
+            break;
+        case 9:
+            selector1.checked = true;
+            selector2.checked = false;
+            selector4.checked = false;
+            selector8.checked = true;
+            break;
+        default:
+            selector1.checked = false;
+            selector2.checked = false;
+            selector4.checked = false;
+            selector8.checked = false;
+            break;
+    }
+
+    function switchCaseNumbers6(time) {
+        switch (time) {
+            case 1:
+                selector1.checked = true;
+                selector2.checked = false;
+                selector4.checked = false;
+                break;
+            case 2:
+                selector1.checked = false;
+                selector2.checked = true;
+                selector4.checked = false;
+                break;
+            case 3:
+                selector1.checked = true;
+                selector2.checked = true;
+                selector4.checked = false;
+                break;
+            case 4:
+                selector1.checked = false;
+                selector2.checked = false;
+                selector4.checked = true;
+                break;
+            case 5:
+                selector1.checked = true;
+                selector2.checked = false;
+                selector4.checked = true;
+                break;
+            case 6:
+                selector1.checked = false;
+                selector2.checked = true;
+                selector4.checked = true;
+                break;
+            default:
+                selector1.checked = false;
+                selector2.checked = false;
+                selector4.checked = false;
+                selector8.checked = false;
+                break;
         }
-
-        updateSecondsLast();
     }
 }
